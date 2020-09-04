1c9285583342db5c5aeecbd0a29c293afa88215a
Simonas Jankauskas <janksimonas@gmail.com>
2020-09-04 12:38:46 +0300

    JAVA operators commit

---
+++ src/lt/simonasjankauskas/app/Operatoriai.java
@@ -0,0 +1,23 @@
+package lt.simonasjankauskas.app;
+
+public class Operatoriai {
+    public static void main (String[] args) {
+
+        // 1. Atsakymas
+        int a = 5;
+        int b = 2;
+
+        System.out.println("Liekana 5 padalinus is dvieju yra:" + (a%b));
+
+        // 2. Atsakymas: gauname error, tačiau jei vietoj int naudosime long o vietoj 0 naudosime 0.0, atspausdintame rezultate gausime Infinity.
+
+        int c = 2;
+        int d = 0;
+
+        System.out.println(c/d);
+
+        // 3. Atsakymai
+
+            // Šito nesupratau...
+    }
+}
